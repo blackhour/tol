e0c31ca30b550798aafc7959ef71a50aeb3faafc
blackhour <40593005+blackhour@users.noreply.github.com>
2018-12-30 13:00:08 +0800

    Add files via upload

--- sceneryServlet.java
+++ sceneryServlet.java
@@ -1,111 +1,118 @@
-/*   
- * Copyright (c) 2010-2020 Founder Ltd. All Rights Reserved.   
- *   
- * This software is the confidential and proprietary information of   
- * Founder. You shall not disclose such Confidential Information   
- * and shall use it only in accordance with the terms of the agreements   
- * you entered into with Founder.   
- *   
- */     
-package servlet.visitor;    
+package servlet.admin;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
-
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.google.gson.Gson;
 
 import pojo.Page;
 import pojo.Scenery;
-import pojo.Ticket;
 import service.ISceneryService;
 import service.impl.SceneryServiceImpl;
 import util.BaseServlet;
-    
-public class sceneryServlet extends BaseServlet {
 
-	ISceneryService iSceneryService=new SceneryServiceImpl();
-	public void select(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+import com.google.gson.Gson;
+
+
+/**
+ * Servlet implementation class SceneryServlet
+ */
+@WebServlet("/SceneryServlet")
+public class sceneryServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private ISceneryService iSceneryService = new SceneryServiceImpl();
+       
+	
+	public void Adelete(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
-		String pageNum= request.getParameter("pageNum");
-		String key="";
-		if(pageNum==null){
-			pageNum="1";
+		String sid = request.getParameter("sid");
+		int res = iSceneryService.Adelete(Integer.parseInt(sid));
+		
+		if (res > 0){
+			response.getWriter().print("{\"res\" : 1}");
+		}
+		else {
+			response.getWriter().print("{\"res\" : 0}");
 		}
 		
-		Page page=new Page();
-		page.setPageNum(Integer.parseInt(pageNum));
-		Page page2=iSceneryService.select(key,page);
+	}
+	
+	
+	public void Aadd(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String address = request.getParameter("address");
+		String describe = request.getParameter("describe");
+	
+		int res = iSceneryService.Aadd(name,address,describe);
+		
+		if (res > 0){
+			response.getWriter().print("{\"res\" : 1}");
+		}
+		else {
+			response.getWriter().print("{\"res\" : 0}");
+		}
+	}
+	
+	public void Aupdate(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String sid = request.getParameter("sid");
+		String sname = request.getParameter("sname");
+		String address = request.getParameter("address");
+		String describe = request.getParameter("describe");
+	
+		int res = iSceneryService.Aupdate(sname,address,describe,Integer.parseInt(sid));
+		
+		if (res > 0){
+			response.getWriter().print("{\"res\" : 1}");
+		}
+		else {
+			response.getWriter().print("{\"res\" : 0}");
+		}
+	}
+	public void selectscenery(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<Scenery> scenery=iSceneryService.selectscenery();
 		Gson gson = new Gson();
-		String strJSON=gson.toJson(page2);
+		String strJSON = gson.toJson(scenery);
+		response.getWriter().print(strJSON);
+			
+	}
+	
+	
+	public void Aselect(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String sname = request.getParameter("sname");
+		String address = request.getParameter("address");
+		
+		
+		String pageNum = request.getParameter("pageNum");
+		if (pageNum == null){
+			pageNum = "1";
+		}
+		
+		Page page = new Page();
+		page.setPageNum(Integer.parseInt(pageNum));
+		
+		Page resPage = iSceneryService.Aselect(sname,address,page);
+		
+		
+		Gson gson = new Gson();
+		String strJSON = gson.toJson(resPage);
 		response.getWriter().print(strJSON);
 	}
-	public void flag(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id=request.getParameter("sid");
-		Scenery scenery=iSceneryService.select(Integer.parseInt(id));
-		
-		HttpSession session =request.getSession();
-		session.setAttribute("scenery", scenery);
-		response.sendRedirect("sceneryDet.jsp");
 	
-	}
-	public void ticket(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id=request.getParameter("sid");
-		Ticket ticket=iSceneryService.ticket(Integer.parseInt(id));
-		HttpSession session =request.getSession();
-		session.setAttribute("ticket", ticket);
-		response.sendRedirect("ticket.jsp");
-	
-	}
-	public void order(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ParseException {
-		String tid=request.getParameter("tid");
-		String sid=request.getParameter("sid");
-		String vid=request.getParameter("vid");
-		String price=request.getParameter("price");
-		String count=request.getParameter("cou");
-		String ticketnum=request.getParameter("ticketnum");
-		String pay=request.getParameter("pay");
-		String number=request.getParameter("number");
-		
-		Date date=new Date();
-		SimpleDateFormat sdf =new SimpleDateFormat( " yyyy-MM-dd HH:mm:ss " );
-		String nowTime = sdf.format(date);
-		Date time =null;
-		try {
-			time = sdf.parse(nowTime);
-		} catch (ParseException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		Ticket ticket=iSceneryService.selectt(Integer.parseInt(tid));
-		if(ticket.getNumber() < Integer.parseInt(ticketnum)){
-			response.sendRedirect("warning.jsp");
-		}else{
-	        int res=iSceneryService.insert(Integer.parseInt(tid),Integer.parseInt(sid),Integer.parseInt(vid),price,count,Integer.parseInt(number),pay,time,Integer.parseInt(ticketnum));
-	        if(res>0){
-	        	response.sendRedirect("order.jsp");
-	        }
-			
-		}
-		
-	}
-	@Override
-	protected void execute(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
 	}
-
-
 
 }
